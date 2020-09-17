37c9bb8d759a3ee47d1fb889d147602723a54182
JesseABolton <notjesse@hotmail.com>
2020-09-18 09:30:48 +1000

    initial commit

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.listen(8080);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-mongoose.connect('mongodb://localhost:27017/movies', function (err) {
+mongoose.connect('mongodb://35.197.182.189:27017/movies', function (err) {
     if (err) {
         return console.log('Mongoose - connection error:', err);
     }
